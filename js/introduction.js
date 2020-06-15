8ca117700acca5de2e65564449578b81646955aa
EgorAntonovich <antonovich.egor1@gmail.com>
2020-06-15 19:00:40 +0300

    Add computed value

--- js/introduction.js
+++ js/introduction.js
@@ -1,6 +1,9 @@
 function AppViewModel(){
     this.firstName = ko.observable("Egor");
     this.secondName = ko.observable("Antonovich");
+    this.fullName = ko.computed(function () {
+        return this.firstName() + " " + this.secondName();
+    }, this);
 }
 
 ko.applyBindings(new AppViewModel());
