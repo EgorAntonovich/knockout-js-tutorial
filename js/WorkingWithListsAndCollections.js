02f4ff89fcccb53fbb43db787f260de044609b54
EAntonovich <EAntonovich@carbonite.com>
2020-07-02 16:21:17 +0300

    WwLaC. Create view and viewmodel

---
+++ js/WorkingWithListsAndCollections.js
@@ -0,0 +1,26 @@
+// Class to represent a row in the seat reservation grid
+function SeatReservation(name, initialMeal) {
+    var self = this;
+    self.name = name;
+    self.meal = ko.observable(initialMeal);
+}
+
+// Overall viewmodel for this screen, along with initial state
+function ReservationsViewModel(){
+    var self = this;
+
+    // Non-editable catalog data - would come from the serve
+    self.availableMeals = [
+        {mealName: "soup", price: 100},
+        {mealName: "cheese", price: 50},
+        {mealName: "tee", price: 10},
+    ];
+
+    // Editable data
+    self.seats = ko.observableArray([
+        new SeatReservation("Steve", self.availableMeals[0]),
+        new SeatReservation("Bob", self.availableMeals[0])
+    ]);
+}
+
+ko.applyBindings(new ReservationsViewModel());
