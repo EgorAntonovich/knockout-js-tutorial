5b745c6545c0e6463640f10733651e0bee14f0a9
EAntonovich <EAntonovich@carbonite.com>
2020-07-05 23:15:07 +0300

    SinglePageApp. Add possibility to make folders selectable

--- js/SinglePageApp.js
+++ js/SinglePageApp.js
@@ -1,7 +1,13 @@
 function WebmailViewModel(){
     //Data
     var self = this;
-    self.folders = ['Inbox', 'Archive', 'Sent', 'Spam']
+    self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
+    self.chosenFolderId = ko.observable();
+
+    // Behaviours
+    self.goToFolder = function(folder){
+        self.chosenFolderId(folder);
+    };
 }
 
 ko.applyBindings(new WebmailViewModel());
